84f6627dc9913d35e6e65678ea097c820ebbc341
Federico 'Magus' Rocca <frocca17@gmail.com>
2022-01-05 17:30:08 -0300

    Raycast shooting
    Disparos con raycast, efecto de muzzle y efecto de impact hit

---
+++ commUNITY/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if( target != null)
+            {
+                target.takeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.3f);
+        }
+    }
+}
